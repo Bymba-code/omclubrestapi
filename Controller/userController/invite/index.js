5e5b0b60e05d2f7b03e6f2a6d2ab1f4303768b43
Bymba-code <bymba6607@gmail.com>
2024-09-14 11:57:26 -0700

    Update index.js

--- Controller/userController/invite/index.js
+++ Controller/userController/invite/index.js
@@ -1,1 +1,98 @@
 
+const { executeQuery } = require("../../../DB/index")
+
+const Invite = async (req, res) => 
+    {
+        try 
+        {
+            const {id, too, cod, create_date, orsonDate, username} = req.body;
+
+
+            if(!id)
+                {
+                    return res.status(403).json(
+                        {
+                            success:false,
+                            data: null,
+                            message: "ID хоосон байна"
+                        }
+                    )
+                }
+
+            const checkQuery = "SELECT * FROM customers WHERE cod = ?"
+            const checkdata = await executeQuery(checkQuery, [cod])
+            
+            if(checkdata.length > 0)
+            {
+                return res.status(403).json(
+                    {
+                        success:false,
+                        data: null,
+                        message: "Бүртгэлтэй код байна"
+                    }
+                )
+            }
+
+            if(!too)
+                {
+                    return res.status(402).json(
+                        {
+                            success:false,
+                            data: null,
+                            message: "Хүний тоог бичнэ үү"
+                        }
+                    )
+                }
+       
+            const isOrson = 0;
+            const isAsuudal = 0;
+            const date = null;
+
+            const values = [
+                id,
+                too,
+                cod,
+                isOrson,
+                isAsuudal,
+                create_date,
+                date,
+                username
+            ]
+
+            const query = "INSERT INTO customers (invited,too,cod,isOrson,isAsuudal,create_date, orsonDate,username) VALUES (?)"
+            
+            const data = await executeQuery(query, [values])
+
+            if(data.affectedRows > 0)
+            {
+                return res.status(200).json(
+                    {
+                        success:true,
+                        data: data,
+                        message:"Урилга амжилттай илгээгдлээ"
+                    })
+            }
+            else 
+            {
+                return res.status(404).json({
+                    success:false,
+                    data: null,
+                    message: "Бүртгэхэд ямар нэгэн алдаа гарлаа"
+                })
+            }
+
+        }
+        catch(err)
+        {
+            return res.status(500).json(
+                {
+                    success:false,
+                    data: null,
+                    message: "Серверийн алдаа та дахин оролдоно уу",
+                    error: err
+                }
+            )
+        }
+    } 
+
+module.exports = Invite

c383c56dbd21c4224b7db7bd0cef8f8efafe1479
Bymba-code <bymba6607@gmail.com>
2024-09-14 12:39:00 -0700

    Update index.js

--- Routes/adminRoutes/index.js
+++ Routes/adminRoutes/index.js
@@ -1,11 +1,14 @@
 const express = require("express")
 const GetAllUsers = require("../../Controller/adminController/ALL_USERS")
 const Delete = require("../../Controller/adminController/DELETE")
+const Customers = require("../../Controller/adminController/Customers")
 
 const router = express.Router()
 
 router.route("/admin/users").get(GetAllUsers)
 
 router.route("/admin/delete/user").post(Delete)
+
+router.route("/admin/customers").get(Customers)
 
 module.exports = router

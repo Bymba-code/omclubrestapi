f122c5c8c63952ef63b4217a7a07f327510232e9
Bymba-code <bymba6607@gmail.com>
2024-09-14 11:58:15 -0700

    Update index.js

--- Routes/userRoutes/index.js
+++ Routes/userRoutes/index.js
@@ -1,11 +1,14 @@
 const express = require("express")
 const CustomersController = require("../../Controller/userController/Customers")
+const Invite = require("../../Controller/userController/invite")
 
 
 const router = express.Router()
 
 
-router.route("/user/customers/:id").get(CustomersController)
+router.route("/user/customers/:user_id").get(CustomersController)
+
+router.route("/user/invite").post(Invite)
 
 
 module.exports = router

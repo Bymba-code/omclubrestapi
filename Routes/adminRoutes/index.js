1c2de2bfd6773b6eb5ebed785683854e760dcd4a
Bymba-code <bymba6607@gmail.com>
2024-09-14 10:56:56 -0700

    Update and rename Routes/index.js to Routes/adminRoutes/index.js

---
+++ Routes/adminRoutes/index.js
@@ -0,0 +1,9 @@
+const express = require("express")
+const GetAllUsers = require("../../Controller/adminController/ALL_USERS")
+
+const router = express.Router()
+
+router.route("/admin/users").post(GetAllUsers)
+
+
+module.exports = router
--- Routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const GetAllUsers = require("../../Controller/adminController/ALL_USERS")
-
-const router = express.Router()
-
-router.route("/admin/users").post(GetAllUsers)
-
-
-module.exports = router

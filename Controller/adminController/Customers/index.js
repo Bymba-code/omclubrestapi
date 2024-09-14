f1d80e2fea1738ffb8157ed93d42dbee87cb3389
Bymba-code <bymba6607@gmail.com>
2024-09-14 12:39:43 -0700

    Update index.js

--- Controller/adminController/Customers/index.js
+++ Controller/adminController/Customers/index.js
@@ -1,1 +1,39 @@
+const { executeQuery } = require("../../../DB/index")
 
+const Customers = async (req, res) => {
+    try 
+    {
+
+        const query = "SELECT * FROM customers"
+        const data = await executeQuery(query)
+
+        if(data.length === 0)
+            {
+                return res.status(403).json({
+                    success:false,
+                    data: null,
+                    message: "Өгөгдөл байхгүй"
+                })
+            }
+            return res.status(200).json(
+                {
+                    success: true,
+                    data:data,
+                    message: "Амжилттай"
+                }
+            )
+
+    }
+    catch(err)
+    {
+        return res.status(500).json(
+            {
+                success:false,
+                data: null,
+                message: "Серверийн алдаа та дахин оролдоно уу"
+            }
+        )
+    }
+}
+
+module.exports = Customers

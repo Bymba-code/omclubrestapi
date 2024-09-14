daea8f9577f55a69e9c1130f3d0190cb6b8e1de9
Bymba-code <bymba6607@gmail.com>
2024-09-14 07:17:24 -0700

    Update index.js

--- Routes/customerRoutes/index.js
+++ Routes/customerRoutes/index.js
@@ -1,9 +1,13 @@
 const express = require("express")
 const GetItems = require("../../Controller/customerController/GET_ALL")
+const userItem = require("../../Controller/customerController/CUSTOMERS")
+
 
 
 const router = express.Router()
 
 router.route("/guard/:id").get(GetItems)
+
+router.route("/customers/:id").get(userItem)
 
 module.exports = router

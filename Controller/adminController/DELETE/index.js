27080694edbb15c5490ac65ead84788e2e316499
Bymba-code <bymba6607@gmail.com>
2024-09-14 11:23:07 -0700

    Update index.js

--- Controller/adminController/DELETE/index.js
+++ Controller/adminController/DELETE/index.js
@@ -1,1 +1,48 @@
 
+const { executeQuery } = require("../../../DB/index");
+
+const Delete = async (req, res) => {
+    try {
+        const { id} = req.body;
+
+        if(!id)
+        {
+            return res.status(403).json(
+                {
+                    success:false,
+                    data: null,
+                    message: "ID байхгүй байна"
+                }
+            )
+        }
+
+        const selectQuery = "SELECT * FROM Users WHERE id = ?";
+        const records = await executeQuery(selectQuery, [id]);
+
+        if (records.length === 0) {
+            return res.status(404).json({
+                success: false,
+                data: null,
+                message: "Хайсан мөр байхгүй байна"
+            });
+        }
+
+       
+        const deleteQ = `DELETE FROM Users WHERE id = ?`;
+        await executeQuery(deleteQ, [id]);
+
+        return res.status(200).json({
+            success: true,
+            data: null,
+            message: "Амжилттай"
+        });
+    } catch (err) {
+        return res.status(500).json({
+            success: false,
+            data: null,
+            message: "Серверийн алдаа, та дахин оролдоно уу"
+        });
+    }
+};
+
+module.exports = Delete;

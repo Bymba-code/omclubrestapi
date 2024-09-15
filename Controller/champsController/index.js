0f8b1465be28e70d8989bc47cf5771334ec4f027
Bymba-code <bymba6607@gmail.com>
2024-09-15 13:40:16 -0700

    Update index.js

--- Controller/champsController/index.js
+++ Controller/champsController/index.js
@@ -1,1 +1,38 @@
+const { executeQuery } = require("../../../DB/index")
 
+
+const ChampionItem = async (req, res) => {
+    try 
+    {
+        const {startDate, endDate} = req.body
+
+        const query = "SELECT * FROM customers WHERE DATE(create_date) BETWEEN ? AND ?"
+
+        const data = await executeQuery(query, [startDate, endDate])
+
+        if(data.length === 0)
+            {
+                return res.status(403).json({
+                    success:false,
+                    data: null,
+                    message: "Өгөгдөл байхгүй"
+                })
+            }
+            return res.status(200).json(
+                {
+                    success: true,
+                    data:data,
+                    message: "Амжилттай"
+                }
+            )
+
+
+
+    }
+    catch(err)
+    {
+        console.log(err)
+    }
+}
+
+module.exports =  ChampionItem 

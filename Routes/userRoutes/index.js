def9da0fb21c091114f7fcbc2a84a04beeac803e
Bymba-code <bymba6607@gmail.com>
2024-09-15 13:17:50 -0700

    Update index.js

--- Routes/userRoutes/index.js
+++ Routes/userRoutes/index.js
@@ -2,6 +2,8 @@ const express = require("express")
 const GetItems = require("../../Controller/customerController/GET_ALL")
 const userItem = require("../../Controller/customerController/CUSTOMERS")
 const Champion = require("../../Controller/championController")
+    const Invite = require("../../Controller/userController/invite")
+
 
 
 
@@ -13,5 +15,8 @@ router.route("/guard/:id").get(GetItems)
 router.route("/customers/:id").get(userItem)
 
 router.route("/champion").get(Champion)
+
+    router.route("/user/invite").post(Invite)
+
 
 module.exports = router

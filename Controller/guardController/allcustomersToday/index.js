f8b2ebc970318f1bfaeb4b7a9a2cf56288829f6f
Bymba-code <bymba6607@gmail.com>
2024-09-15 15:58:37 -0700

    Update index.js

--- Controller/guardController/allcustomersToday/index.js
+++ Controller/guardController/allcustomersToday/index.js
@@ -1,12 +1,39 @@
-const { executeQuery } = require("../../../DB/index");
+const { executeQuery } = require("../../../DB/index")
 
 const customerControllerGuard = async (req, res) => {
-    try {
-      res.status(200).json({data:"ho"})
-    } catch (err) {
-        console.error('Error executing query:', err);
-           return res.json(error)
+    try 
+    {
+
+        const query = "SELECT * FROM customers WHERE DATE(create_date) = CURDATE()"
+        const data = await executeQuery(query)
+
+        if(data.length === 0)
+            {
+                return res.status(403).json({
+                    success:false,
+                    data: null,
+                    message: "Өгөгдөл байхгүй"
+                })
+            }
+            return res.status(200).json(
+                {
+                    success: true,
+                    data:data,
+                    message: "Амжилттай"
+                }
+            )
+
+    }
+    catch(err)
+    {
+        return res.status(500).json(
+            {
+                success:false,
+                data: null,
+                message: "Серверийн алдаа та дахин оролдоно уу"
+            }
+        )
     }
 }
 
-module.exports = customerControllerGuard;
+module.exports = customerControllerGuard

e391950a27020578ecc18f093f7a1422c415fcb4
Bymba-code <bymba6607@gmail.com>
2024-09-15 02:08:08 -0700

    Update index.js

--- Controller/guardController/allcustomersToday/index.js
+++ Controller/guardController/allcustomersToday/index.js
@@ -2,22 +2,7 @@ const { executeQuery } = require("../../../DB/index");
 
 const customerControllerGuard = async (req, res) => {
     try {
-        const query = "SELECT * FROM customers";
-        const data = await executeQuery(query);
-        
-        if (data.length === 0) {
-            return res.status(403).json({
-                success: false,
-                data: null,
-                message: "Өгөгдөл байхгүй" 
-            });
-        }
-        
-        return res.status(200).json({
-            success: true,
-            data: data,
-            message: "Амжилттай" 
-        });
+      res.status(200).json({data:"ho"})
     } catch (err) {
         console.error('Error executing query:', err);
         return res.status(500).json({

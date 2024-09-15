b22edcec92009d0fd5a1dfb5f9d5772f79f48d7f
Bymba-code <bymba6607@gmail.com>
2024-09-15 08:16:58 -0700

    Update index.js

--- Controller/userController/temtseen/index.js
+++ Controller/userController/temtseen/index.js
@@ -1,1 +1,19 @@
-const { executeQuery } = require("../../../DB/index"); const Temtseen = async (req, res) => { try { const query = "SELECT * FROM champion"; const data = await executeQuery(query); if (data.length === 0) { return res.status(404).json({ success: false, data: null, message: "Амжилтгүй" }); } return res.status(200).json({ success: true, data: data, message: "Амжилттай" }); } catch (err) { return res.status(500).json({ success: false, data: null, message: "Серверийн алдаа та дахин оролдоно уу" }); } }; module.exports = Temtseen;
+const {executeQuery} = require("../../../DB/index");
+const Temtseen = async (req, res) => {
+    try {
+        const query = "SELECT * FROM champion";
+        const data = await executeQuery(query);
+        return res.status(200).json({
+            success: true,
+            data: data,
+            message: "Амжилттай"
+        });
+    } catch (err) {
+        return res.status(500).json({
+            success: false,
+            data: null,
+            message: "Серверийн алдаа та дахин оролдоно уу"
+        });
+    }
+};
+module.exports = Temtseen;

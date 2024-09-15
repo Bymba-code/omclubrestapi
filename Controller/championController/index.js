cac86ebcf75304258b2752cb471843cd96df6965
Bymba-code <bymba6607@gmail.com>
2024-09-15 08:19:53 -0700

    Update index.js

--- Controller/championController/index.js
+++ Controller/championController/index.js
@@ -1,1 +1,19 @@
-
+const {executeQuery} = require("../../../DB/index");
+const Champion = async (req, res) => {
+    try {
+        const query = "SELECT * FROM champion";
+        const data = await executeQuery(query);
+        return res.status(200).json({
+            success: true,
+            data: data,
+            message: "Амжилттай"
+        });
+    } catch (err) {
+        return res.status(500).json({
+            success: false,
+            data: null,
+            message: "Серверийн алдаа та дахин оролдоно уу"
+        });
+    }
+};
+module.exports = Champion;

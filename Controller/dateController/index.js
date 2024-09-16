2decdc500bae1a85a714fc1b74fbb8f08c4769bb
Bymba-code <bymba6607@gmail.com>
2024-09-16 11:12:11 -0700

    Update index.js

--- Controller/dateController/index.js
+++ Controller/dateController/index.js
@@ -1,1 +1,34 @@
+const { executeQuery } = require("../../../DB/index")
 
+const getTime = (req,res) => {
+  try 
+  {
+    const query = "SELECT * FROM date_negtgel"
+    const data = await executeQuery(query)
+    if(data.length === 0)
+    {
+        return res.status(404).json({
+                    success:false,
+                    data: null,
+                    message: "Алдаа гарлаа"
+                })
+    }
+    return res.status(200).json({
+        success:true,
+        data:data,
+        message: "Амжилттай татлаа"
+    })
+    
+  }
+  catch(err)
+  {
+    return res.status(500).json(
+                {
+                    success:false,
+                    data: null,
+                    message: "Серверийн алдаа та дахин оролдоно уу",
+                    error: err
+                }
+            )
+  }
+}

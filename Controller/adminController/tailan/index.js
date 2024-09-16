74f8d089ab4943737b656f66ee67c58a32281552
Bymba-code <bymba6607@gmail.com>
2024-09-16 17:06:58 +0800

    Update index.js

--- Controller/adminController/tailan/index.js
+++ Controller/adminController/tailan/index.js
@@ -1,1 +1,16 @@
+const ExcelJS = require('exceljs');
+
+const tailan = async (req, res) => {
+    const workbook = new ExcelJS.Workbook();
+    const worksheet = workbook.addWorksheet('Sheet1');
+
+    worksheet.addRow(['Name', 'Age']);
+    worksheet.addRow(['John Doe', 30]);
+
+    const buffer = await workbook.xlsx.writeBuffer();
+    
+    res.setHeader('Content-Disposition', 'attachment; filename="report.xlsx"');
+    res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+    res.send(buffer);
+});
 

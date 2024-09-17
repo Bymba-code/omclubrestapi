074b67b71b07b74d0bec40aacd74ba3155bcb543
Bymba-code <bymba6607@gmail.com>
2024-09-17 14:34:35 -0700

    Update index.js

--- Controller/adminController/DETAIL_USER/index.js
+++ Controller/adminController/DETAIL_USER/index.js
@@ -1,19 +1,16 @@
 const { executeQuery } = require("../../../DB/index");
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcrypt");
 
 const getDetail = async (req, res) => {
     try {
-        const {username} = req.body;
+        const { username, password } = req.body; // Assume plain-text password is sent in request body
 
-        if(!username)
-        {
-            return res.status(403).json(
-                {
-                    success:false,
-                    data: null,
-                    message: "Ажилтаны ажиллаж буй нэрийг явуулна уу"
-                }
-            )
+        if (!username || !password) {
+            return res.status(403).json({
+                success: false,
+                data: null,
+                message: "Ажилтаны ажиллаж буй нэр болон нууц үг шаардлагатай"
+            });
         }
 
         const selectQuery = "SELECT * FROM Users WHERE username = ?";
@@ -28,14 +25,23 @@ const getDetail = async (req, res) => {
         }
 
         const user = records[0];
+        const hashedPassword = user.password; // Assume hashed password is stored in 'password' field
 
-        const password = bcrypt.compareSync(user.password)
+        // Compare the provided plain-text password with the stored hashed password
+        const passwordMatch = bcrypt.compareSync(password, hashedPassword);
+
+        if (!passwordMatch) {
+            return res.status(401).json({
+                success: false,
+                data: null,
+                message: "Нууц үг буруу байна"
+            });
+        }
 
         return res.status(200).json({
             success: true,
             data: records,
-            message: "Амжилттай",
-            password:password
+            message: "Амжилттай"
         });
     } catch (err) {
         return res.status(500).json({

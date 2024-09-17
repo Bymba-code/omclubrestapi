db96c7447c12f6da18f0dcaf5159fdce1084f16b
Bymba-code <bymba6607@gmail.com>
2024-09-17 10:39:34 -0700

    Update index.js

--- Controller/dateController/index.js
+++ Controller/dateController/index.js
@@ -28,6 +28,31 @@ const getDate = async (req, res) => {
   }
 };
 
+const insertDate = (req, res) => {
+  try 
+  {
+    const {date, changeTime} = req.body;
+
+    const query = `UPDATE date_negtgel SET date = ?, change_date = ? WHERE id = ?`;
+
+    const result = await executeQuery(query, [date, changeTime]);
+    return res.status(200).json({
+      success: true,
+      data: null,
+      message: "Амжилттай шинэчиллээ" // "Successfully updated"
+    });
+    
+  }
+  catch(err)
+  {
+    return res.status(500).json({
+      success: false,
+      data: null,
+      message: "Серверийн алдаа та дахин оролдоно уу", 
+      error: err
+    });
+  }
+}
 
 
 

59e3eef15653b029a5a3d50ba9718a99397fc1cb
Bymba-code <bymba6607@gmail.com>
2024-09-17 14:23:19 -0700

    Update index.js

--- Routes/adminRoutes/index.js
+++ Routes/adminRoutes/index.js
@@ -5,12 +5,16 @@ const Customers = require("../../Controller/adminController/Customers")
 const insertChamp = require("../../Controller/addChamp")
 const tailan = require("../../Controller/adminController/tailan")
 const {getDate, insertDate}= require("../../Controller/dateController/index.js")
+const getDetail = require("../../Controller/adminController/DETAIL_USER")
+
 
 const router = express.Router()
 
 router.route("/admin/users").get(GetAllUsers)
 
 router.route("/admin/delete/user").post(Delete)
+
+
 
 router.route("/admin/customers").get(Customers)
 
@@ -19,5 +23,8 @@ router.route("/admin/champs").post(insertChamp)
 router.route("/admin/tailan").get(tailan)
 
 router.route("/admin/date").get(getDate).post(insertDate)
+
+router.route("/admin/user").post(getDetail)
+
 
 module.exports = router

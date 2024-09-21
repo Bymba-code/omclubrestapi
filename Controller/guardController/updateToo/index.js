0fc4e12d60a865e739466a2e0c451746e6b4ea61
Bymba-code <bymba6607@gmail.com>
2024-09-21 20:59:21 +0800

    Update index.js

--- Controller/guardController/updateToo/index.js
+++ Controller/guardController/updateToo/index.js
@@ -1,1 +1,37 @@
+const { executeQuery } = require("../../../DB/index");
 
+const updateToo = async (req, res) => {
+    try {
+        const {itemId} = req.body;
+
+        if(!itemId)
+        {
+            return res.status(403).json(
+                {
+                    success:false,
+                    data: null,
+                    message: "кодийг явуулна уу"
+                }
+            )
+        }
+        
+
+       
+        const updateQuery = `UPDATE customers SET too ? WHERE id = ?`;
+        await executeQuery(updateQuery, [itemId]);
+
+        return res.status(200).json({
+            success: true,
+            data: null,
+            message: "Амжилттай"
+        });
+    } catch (err) {
+        return res.status(500).json({
+            success: false,
+            data: null,
+            message: "Серверийн алдаа, та дахин оролдоно уу"
+        });
+    }
+};
+
+module.exports = updateToo;

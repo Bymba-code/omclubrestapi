c1dfd4f3735e7c60f7976a9804ebc9bb9e4a5704
Bymba-code <bymba6607@gmail.com>
2024-09-21 21:04:32 +0800

    Update index.js

--- Routes/guardRoutes/index.js
+++ Routes/guardRoutes/index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const customerControllerGuard = require("../../Controller/guardController/allcustomersToday")
 const oruulahController = require("../../Controller/guardController/oruulahController")
 const asuudalContainer = require("../../Controller/guardController/asuudalContainer")
+const updateToo = require("../../Controller/guardController/updateToo")
 
 const router = express.Router()
 
@@ -10,5 +11,7 @@ router.route("/guard/customers").get(customerControllerGuard)
 router.route("/guard/oruulah").post(oruulahController)
 
 router.route("/guard/asuudal").post(asuudalContainer)
+
+router.route("/guard/update").post(updateToo)
 
 module.exports = router

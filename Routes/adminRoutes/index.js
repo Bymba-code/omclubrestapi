1a88c445e64c762243ca7e318c0bbddb11b48769
Bymba-code <bymba6607@gmail.com>
2024-09-21 20:25:49 +0800

    Update index.js

--- Routes/adminRoutes/index.js
+++ Routes/adminRoutes/index.js
@@ -1,43 +1,27 @@
-const express = require("express")
-const GetAllUsers = require("../../Controller/adminController/ALL_USERS")
-const Delete = require("../../Controller/adminController/DELETE")
-const Customers = require("../../Controller/adminController/Customers")
-const insertChamp = require("../../Controller/addChamp")
-const tailan = require("../../Controller/adminController/tailan")
-const {getDate, insertDate}= require("../../Controller/dateController/index.js")
-const getDetail = require("../../Controller/adminController/DETAIL_USER")
-const getDifference = require("../../Controller/statisticController")
-const  dataDashboard = require("../../Controller/adminController/statis")
-const getStory = require("../../Controller/adminController/storyInvite")
-const storyInvite = require("../../Controller/adminController/addStory")
+const express = require("express");
+const GetAllUsers = require("../../Controller/adminController/ALL_USERS");
+const Delete = require("../../Controller/adminController/DELETE");
+const Customers = require("../../Controller/adminController/Customers");
+const insertChamp = require("../../Controller/addChamp");
+const tailan = require("../../Controller/adminController/tailan");
+const { getDate, insertDate } = require("../../Controller/dateController/index.js");
+const getDetail = require("../../Controller/adminController/DETAIL_USER");
+const getDifference = require("../../Controller/statisticController");
+const dataDashboard = require("../../Controller/adminController/statis");
+const getStory = require("../../Controller/adminController/storyInvite");
+const storyInvite = require("../../Controller/adminController/addStory");
 
+const router = express.Router();
 
+router.route("/admin/users").get(GetAllUsers);
+router.route("/admin/delete/user").post(Delete);
+router.route("/admin/customers").get(Customers);
+router.route("/admin/champs").post(insertChamp);
+router.route("/admin/tailan").get(tailan);
+router.route("/admin/date").get(getDate).post(insertDate);
+router.route("/admin/user").post(getDetail);
+router.route("/admin/stats").get(getDifference);
+router.route("/admin/dashboard").get(dataDashboard);
+router.route("/story").get(getStory).post(storyInvite);
 
-const router = express.Router()
-
-router.route("/admin/users").get(GetAllUsers)
-
-router.route("/admin/delete/user").post(Delete)
-
-
-
-router.route("/admin/customers").get(Customers)
-
-router.route("/admin/champs").post(insertChamp)
-
-router.route("/admin/tailan").get(tailan)
-
-router.route("/admin/date").get(getDate).post(insertDate)
-
-router.route("/admin/user").post(getDetail)
-
-router.route("/admin/stats").get(getDifference)
-
-router.route("/admin/dashboard").get(dataDashboard)
-
-router.route("/story").get(getStory).post(storyInvite)
-
-
-
-
-module.exports = router
+module.exports = router; // Ensure this line ends with a semicolon
